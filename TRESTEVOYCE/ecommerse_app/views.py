1d375da612d61f9941c5ba5a7b6f84bd10feec7e
Mark Steven Camposano <markstevencamposano409@gmail.com>
2025-12-05 19:01:26 -0800

    fully added the user controller

--- TRESTEVOYCE/ecommerse_app/views.py
+++ TRESTEVOYCE/ecommerse_app/views.py
@@ -17,6 +17,7 @@ class UserController:
             self.phone_number = request.POST.get('phone_number')
             self.barangay = request.POST.get('barangay')
             self.munucipality_or_city = request.POST.get('munucipality_or_city')
+            self.province = request.POST.get('province')
             self.region = request.POST.get('region')
             self.is_seller = request.POST.get('is_seller',False)
             self.profile_picture = request.FILES.get('profile_picture')
@@ -25,7 +26,11 @@ class UserController:
             if self.phone_number == None:
                 return JsonResponse({'error':'phone number must not be none'})
             
-            phone_number = int(self.phone_number)
+            try:
+                phone_number = int(self.phone_number)
+            
+            except ValueError:
+                return JsonResponse({'error':'value must be a integer'})
         
             self.user = User.objects.create(
                 first_name = self.first_name,
@@ -33,15 +38,59 @@ class UserController:
                 phone_number = phone_number,
                 barangay = self.barangay,
                 municipality_or_city = self.munucipality_or_city,
+                province = self.province,
                 region = self.region,
                 is_seller = False,
             )
-        if self.profile_picture:
-            self.user.profile_picture = self.profile_picture
-            self.user.save()
+            if self.profile_picture:
+                self.user.profile_picture = self.profile_picture
+                self.user.save()
 
         
         return JsonResponse({'message':'data has been successfully added'})
+
+    def edit_user(self,request,id):
+        
+        user = get_object_or_404(User,id = id)
+
+        if request.method == 'POST':
+
+            user.first_name = request.POST.get('first_name',user.first_name)
+            user.last_name = request.POST.get('last_name',user.last_name)
+            user.email_address = request.POST.get('email_address',user.email_address)
+            user.phone_number = request.POST.get('phone_number',user.phone_number)
+            user.barangay = request.POST.get('barangay',user.barangay)
+            user.municipality_or_city = request.POST.get('municipality_or_city',user.municipality_or_city)
+            user.province = request.POST.get('province',user.province)
+            user.region = request.POST.get('region',user.region)
+
+            profile_picture = request.FILES.get('profile_picture')
+            if profile_picture:
+                user.profile_picture = profile_picture
+
+            if user.phone_number == None:
+                return JsonResponse({'error':'phone number must not be none'})
+            
+            try:
+                user.phone_number = int(user.phone_number)
+            
+            except ValueError:
+                return JsonResponse({'error':'value must be a integer'})
+            
+            user.save()
+
+            return JsonResponse({'message': 'User updated successfully'})
+
+    def remove_user(self,request,id):
+
+        if request.method == 'POST':    
+
+            user = get_object_or_404(User, id = id)
+            user.delete()
+            return JsonResponse({'message':'Successfully Deleted'})
+
+        return JsonResponse({'error':'Failed to Delete Data'})
+
 
 #this class will handle all of the CRUD methods for the Store model
 
@@ -84,3 +133,4 @@ class StoreController:
                 self.store.save()
 
             return JsonResponse({'message':'data has been successfully added'})
+        

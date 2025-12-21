bc0fdd3cda730f2e6b6a9d810fddd4a1d45f6bd4
KramNevets <hitsugayasubaru@gmail.com>
2025-12-20 19:44:03 -0800

    fixed user  cotroller bugs

--- TRESTEVOYCE/ecommerse_app/views_controllers/user_views.py
+++ TRESTEVOYCE/ecommerse_app/views_controllers/user_views.py
@@ -1,96 +1,84 @@
-from django.http import JsonResponse,HttpResponse
-from ..models import User,Store
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from ..models import User
+
+@csrf_exempt
+def create_user(request):
+    if request.method != "POST":
+        return JsonResponse({"error": "POST required"}, status=405)
+
+    first_name = request.POST.get("first_name")
+    last_name = request.POST.get("last_name")
+    email = request.POST.get("email_address")
+    phone = request.POST.get("phone_number")
+    barangay = request.POST.get("barangay")
+    city = request.POST.get("municipality_or_city")
+    province = request.POST.get("province")
+    region = request.POST.get("region")
+    is_seller = request.POST.get("is_seller") == "true"
+
+    if not first_name or not last_name or not email:
+        return JsonResponse({"error": "Missing required fields"}, status=400)
+
+    user = User.objects.create(
+        first_name=first_name,
+        last_name=last_name,
+        email_address=email,
+        phone_number=phone,
+        barangay=barangay,
+        municipality_or_city=city,
+        province=province,
+        region=region,
+        is_seller=is_seller
+    )
+
+    return JsonResponse({"message": "User created", "id": user.id}, status=201)
 
 
-#this class will handle all of the CRUD methods for the user model
+def list_users(request):
+    if request.method != "GET":
+        return JsonResponse({"error": "GET required"}, status=405)
 
-class UserController:
-    
-    def create_user(self,request):
-        
-        if request.method == 'POST':
-
-            self.first_name = request.POST.get('first_name')
-            self.last_name = request.POST.get('last_name')
-            self.email_address =request.POST.get('email_address')
-            self.phone_number = request.POST.get('phone_number')
-            self.barangay = request.POST.get('barangay')
-            self.munucipality_or_city = request.POST.get('munucipality_or_city')
-            self.province = request.POST.get('province')
-            self.region = request.POST.get('region')
-            self.is_seller = request.POST.get('is_seller',False)
-            self.profile_picture = request.FILES.get('profile_picture')
+    users = User.objects.all()
+    data = [
+        {
+            "id": u.id,
+            "name": f"{u.first_name} {u.last_name}",
+            "email": u.email_address,
+            "is_seller": u.is_seller
+        }
+        for u in users
+    ]
+    return JsonResponse(data, safe=False)
 
 
-            if self.phone_number == None:
-                return JsonResponse({'error':'phone number must not be none'})
-            
-            try:
-                phone_number = int(self.phone_number)
-            
-            except ValueError:
-                return JsonResponse({'error':'value must be a integer'})
-        
-            self.user = User.objects.create(
-                first_name = self.first_name,
-                last_name = self.last_name,
-                phone_number = phone_number,
-                barangay = self.barangay,
-                municipality_or_city = self.munucipality_or_city,
-                province = self.province,
-                region = self.region,
-                is_seller = False,
-            )
-            if self.profile_picture:
-                self.user.profile_picture = self.profile_picture
-                self.user.save()
+@csrf_exempt
+def update_user(request, user_id):
+    if request.method not in ["PUT", "PATCH"]:
+        return JsonResponse({"error": "PUT or PATCH required"}, status=405)
 
-        
-        return JsonResponse({'message':'data has been successfully added'})
+    user = get_object_or_404(User, pk=user_id)
+    user.first_name = request.POST.get("first_name", user.first_name)
+    user.last_name = request.POST.get("last_name", user.last_name)
+    user.email_address = request.POST.get("email_address", user.email_address)
+    user.phone_number = request.POST.get("phone_number", user.phone_number)
+    user.barangay = request.POST.get("barangay", user.barangay)
+    user.municipality_or_city = request.POST.get("municipality_or_city", user.municipality_or_city)
+    user.province = request.POST.get("province", user.province)
+    user.region = request.POST.get("region", user.region)
+    user.is_seller = request.POST.get("is_seller", str(user.is_seller)) == "true"
+    user.save()
 
-    def edit_user(self,request,id):
-        
-        user = get_object_or_404(User,id = id)
-
-        if request.method == 'POST':
-
-            user.first_name = request.POST.get('first_name',user.first_name)
-            user.last_name = request.POST.get('last_name',user.last_name)
-            user.email_address = request.POST.get('email_address',user.email_address)
-            user.phone_number = request.POST.get('phone_number',user.phone_number)
-            user.barangay = request.POST.get('barangay',user.barangay)
-            user.municipality_or_city = request.POST.get('municipality_or_city',user.municipality_or_city)
-            user.province = request.POST.get('province',user.province)
-            user.region = request.POST.get('region',user.region)
-
-            profile_picture = request.FILES.get('profile_picture')
-            if profile_picture:
-                user.profile_picture = profile_picture
-
-            if user.phone_number == None:
-                return JsonResponse({'error':'phone number must not be none'})
-            
-            try:
-                user.phone_number = int(user.phone_number)
-            
-            except ValueError:
-                return JsonResponse({'error':'value must be a integer'})
-            
-            user.save()
-
-            return JsonResponse({'message': 'User updated successfully'})
-
-    def remove_user(self,request,id):
-
-        if request.method == 'POST':    
-
-            user = get_object_or_404(User, id = id)
-            user.delete()
-            return JsonResponse({'message':'Successfully Deleted'})
-
-        return JsonResponse({'error':'Failed to Delete Data'})
+    return JsonResponse({"message": "User updated"})
 
 
+@csrf_exempt
+def delete_user(request, user_id):
+    if request.method != "DELETE":
+        return JsonResponse({"error": "DELETE required"}, status=405)
 
-        
+    user = get_object_or_404(User, pk=user_id)
+    user.delete()
+    return JsonResponse({"message": "User deleted"})

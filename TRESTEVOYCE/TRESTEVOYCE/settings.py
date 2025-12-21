f64e087bc7a482e70030f58565b3352a2fa13ad7
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2025-12-22 00:09:32 +0800

    Nakkatanga na pag ayad hini na LOGIN and SIGNUP

--- TRESTEVOYCE/TRESTEVOYCE/settings.py
+++ TRESTEVOYCE/TRESTEVOYCE/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "ecommerse_app" / "static",
+    BASE_DIR / "static",
 ]
 
 #this is where you store the images uploaded to database
--- TRESTEVOYCE/ecommerse_app/views.py
+++ TRESTEVOYCE/ecommerse_app/views.py
@@ -1,28 +1,43 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 def login_view(request):
     return render(request, 'vendor/login.html')
 
+
 def signup_view(request):
     return render(request, 'vendor/signup.html')
 
+
 def base_view(request):
-    return render(request, 'vendor/base.html')
+    return redirect('dashboard')
+
 
 def dashboard_view(request):
     return render(request, 'vendor/dashboard.html')
 
+
 def products_view(request):
     return render(request, 'vendor/products.html')
+
 
 def orders_view(request):
     return render(request, 'vendor/orders.html')
 
+
 def analytics_view(request):
-    return render(request, 'vendor/analytics.html')
+    return render(request, 'vendoranalytics.html')
+
 
 def settings_view(request):
     return render(request, 'vendor/settings.html')
 
+
 def notifications_view(request):
     return render(request, 'vendor/notifications.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')

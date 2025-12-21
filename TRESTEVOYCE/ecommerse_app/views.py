fc1add3ae68a73f0143cfa83ba33e6924f550fb3
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2025-12-21 19:19:04 +0800

    Trying to connect each web page

--- TRESTEVOYCE/ecommerse_app/views.py
+++ TRESTEVOYCE/ecommerse_app/views.py
@@ -1,28 +1,28 @@
 from django.shortcuts import render, redirect
 
 def login_view(request):
-    return render(request, 'login.html')
+    return render(request, 'vendor/login.html')
 
 def signup_view(request):
-    return render(request, 'signup.html')
+    return render(request, 'vendor/signup.html')
 
 def base_view(request):
-    return render(request, 'base.html')
+    return render(request, 'vendor/base.html')
 
 def dashboard_view(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'vendor/dashboard.html')
 
 def products_view(request):
-    return render(request, 'products.html')
+    return render(request, 'vendor/products.html')
 
 def orders_view(request):
-    return render(request, 'orders.html')
+    return render(request, 'vendor/orders.html')
 
 def analytics_view(request):
-    return render(request, 'analytics.html')
+    return render(request, 'vendor/analytics.html')
 
 def settings_view(request):
-    return render(request, 'settings.html')
+    return render(request, 'vendor/settings.html')
 
 def notifications_view(request):
-    return render(request, 'notifications.html')
+    return render(request, 'vendor/notifications.html')

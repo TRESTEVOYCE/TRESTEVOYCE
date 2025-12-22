816fdee438e25be5ba42ea71bf72b2d19d6baead
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2025-12-22 21:13:07 +0800

    adding admin configurations

--- TRESTEVOYCE/ecommerse_app/admin.py
+++ TRESTEVOYCE/ecommerse_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Customer, Order, Cart, Inventory, Ordered_Item, Store, UserProfile
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(Inventory)
+admin.site.register(Ordered_Item)
+admin.site.register(Store)
+admin.site.register(UserProfile)

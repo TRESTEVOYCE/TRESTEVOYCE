7d4153add9f5c3ccc393448d6ae2f4798f217f9a
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2025-12-23 17:47:29 +0800

    Added Category class and set it to Null

--- TRESTEVOYCE/ecommerse_app/models.py
+++ TRESTEVOYCE/ecommerse_app/models.py
@@ -5,10 +5,19 @@ from django.contrib.auth.models import User
 # Main Models
 # ------------------------
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)  # Optional inventory field
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
 
     def __str__(self):
         return self.name

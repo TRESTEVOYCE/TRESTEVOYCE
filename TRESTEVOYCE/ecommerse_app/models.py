44d8dc2cf11f4f4e621ff93fee32fc4e72a959ca
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2025-12-23 14:47:23 +0800

    Fixes null value for Cart class

--- TRESTEVOYCE/ecommerse_app/models.py
+++ TRESTEVOYCE/ecommerse_app/models.py
@@ -44,7 +44,7 @@ class Order(models.Model):
 # ------------------------
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
 
